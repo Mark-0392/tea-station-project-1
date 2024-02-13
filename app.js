0be3e9b5e0c9463f34ba1f6a82a99179bc4b3568
Mark-0392 <itsmxd03gmail.com>
2024-02-13 22:05:18 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// setup nav
+const navBtn = document.getElementById('nav-btn')
+const navbar = document.getElementById('navbar')
+const navClose = document.getElementById('nav-close')
+// show nav
+navBtn.addEventListener('click', () => {
+  navbar.classList.add('show-nav')
+})
+// close nav
+navClose.addEventListener('click', () => {
+  navbar.classList.remove('show-nav')
+})
+//setup date
+const date = (document.getElementById('date').innerHTML =
+  new Date().getFullYear())
